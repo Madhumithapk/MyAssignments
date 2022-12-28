eb261906c015c668a94f7bff04bf2f86bba27e76
Madhumithapk <121346464+Madhumithapk@users.noreply.github.com>
2022-12-28 19:59:58 +0530

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    for (int i = 0; i <=30; i++) {
+		if(i%2==0)
+		System.out.println(i);
+	}
+	}
+
+}
---
+++ Voting.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int age=20;
+    if(age>18) {
+    System.out.println("Eligible to vote");
+    }
+    else if (age<18) {
+		System.out.println("Not eligible to vote");
+	}
+	}
+}
+
